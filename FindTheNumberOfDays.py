a7932ae27852206ff6e90cb7c7d6f2753c673562
Winged Demsar <winged.demsar2@gmail.com>
2022-10-19 17:08:05 -0400

    finished

--- FindTheNumberOfDays.py
+++ FindTheNumberOfDays.py
@@ -1,8 +1,6 @@
-# Program that will be able to count the number of days of a specific weekday for a specific year and month
-
-# Menu for days list, ask for year, ask for month, need to error check user
-
 import calendar
+
+# A function that gathers the user's input toward what weekday they want to choose, if the input is not a number between 0 - 6 then there is an error prompt and takes user back to menu screen of all days of the week
 
 
 def GatherWeekDayInput():
@@ -10,7 +8,7 @@ def GatherWeekDayInput():
     while True:
         try:
             userInput = str(input(
-                "Which day of the week do you want to count?\n0: Monday\n1: Tuesday\n2: Wednesday\n3: Thursday\n4: Friday\n5: Saturday\n6: Sunday\nOr 'exit' to quit\n? "))
+                "\nWhich day of the week do you want to count?\n0: Monday\n1: Tuesday\n2: Wednesday\n3: Thursday\n4: Friday\n5: Saturday\n6: Sunday\nOr 'exit' to quit\n? "))
             if userInput == "exit":
                 exit()
             else:
@@ -21,6 +19,8 @@ def GatherWeekDayInput():
                     return userWeekDayInput
         except ValueError:
             print("Please only input a number between 0 - 6!")
+
+# A function that ask for user's year input
 
 
 def GatherYearInput():
@@ -33,6 +33,8 @@ def GatherYearInput():
         except ValueError:
             print("Sorry, that's not valid input.")
             GatherWeekDayInput()
+
+# A function that ask for user's month input
 
 
 def GatherMonthInput():
@@ -48,15 +50,105 @@ def GatherMonthInput():
             print("Sorry, that's not valid input.")
             GatherWeekDayInput()
 
+# A function that loops the program until the user's input equals 'exit'
+
 
 def LoopDecision():
     if userWeekDayInput == "exit":
         return False
 
+# This functions converts the number input of user from the selected month into the corresponding month in letters
 
+
+def ChangeMonthInput(monthInput):
+    if monthInput == 1:
+        return "January"
+    if monthInput == 2:
+        return "February"
+    if monthInput == 3:
+        return "March"
+    if monthInput == 4:
+        return "April"
+    if monthInput == 5:
+        return "May"
+    if monthInput == 6:
+        return "June"
+    if monthInput == 7:
+        return "July"
+    if monthInput == 8:
+        return "August"
+    if monthInput == 9:
+        return "September"
+    if monthInput == 10:
+        return "October"
+    if monthInput == 11:
+        return "November"
+    if monthInput == 12:
+        return "December"
+
+
+def ChangeWeekDayInput(weekDayInput):
+    if weekDayInput == 0:
+        return "Mondays"
+    if weekDayInput == 1:
+        return "Tuesdays"
+    if weekDayInput == 2:
+        return "Wednesdays"
+    if weekDayInput == 3:
+        return "Thursdays"
+    if weekDayInput == 4:
+        return "Fridays"
+    if weekDayInput == 5:
+        return "Saturdays"
+    if weekDayInput == 6:
+        return "Sundays"
+
+# This function uses the weekday input of the user to return the calender day of the corresponding number
+
+
+def FindWeekDay(weekDayInput):
+    if weekDayInput == 0:
+        return calendar.MONDAY
+    if weekDayInput == 1:
+        return calendar.TUESDAY
+    if weekDayInput == 2:
+        return calendar.WEDNESDAY
+    if weekDayInput == 3:
+        return calendar.THURSDAY
+    if weekDayInput == 4:
+        return calendar.FRIDAY
+    if weekDayInput == 5:
+        return calendar.SATURDAY
+    if weekDayInput == 6:
+        return calendar.SUNDAY
+
+# This functions uses all variables gathered from previous function to find the outcome of the user's input with a for loop
+
+
+def Results(weekDaysChange, weekDaysInput, monthChange, monthInput, yearInput):
+    weekDays = calendar.monthcalendar(yearInput, monthInput)
+
+    # this adds a one every time a weekday doesn't equal 0, if it equals 0 then thats not the user's choice of weekday
+    outcome = sum(1 for x in weekDays if x[weekDaysInput] != 0)
+
+    print("\nThere are", outcome, weekDaysChange, "in the month of",
+          monthChange, "and in the year of", yearInput, ".")
+    print("-----------")
+
+
+# This must have the while loop to loop all functions of the program with their respected variables
 while (LoopDecision):
     userWeekDayInput = GatherWeekDayInput()
 
     userYearInput = GatherYearInput()
 
     userMonthInput = GatherMonthInput()
+
+    monthChange = ChangeMonthInput(userMonthInput)
+
+    weekDayChange = ChangeWeekDayInput(userWeekDayInput)
+
+    findCalenderWeekDay = FindWeekDay(userWeekDayInput)
+
+    Results(weekDayChange, userWeekDayInput,
+            monthChange, userMonthInput, userYearInput)
--- pseudocode.md
+++ pseudocode.md
@@ -4,25 +4,31 @@
 
 ### User Interface =
 
-1.
-2.
-3.
+1. Choose what day of the week they want to use between 0 - 6
+2. Input a year they want to do it with
+3. Input a month they want
 4.
 
 ### Code Interface =
 
 ##### Functions :
 
-1.
-2.
-3.
-4.
+1. A function that gathers the user's input toward what weekday they want to choose, if the input is not a number between 0 - 6 then there is an error prompt and takes user back to menu screen of all days of the week
+2. A function that ask for user's year input
+3. A function that ask for user's month input
+4. A function that loops the program until the user's input equals 'exit'
+5. This functions converts the number input of user from the selected month into the corresponding month in letters
+6. This functions converts the number input of user from the selected weekday into the corresponding weekday in letters
+7. This function uses the weekday input of the user to return the calender day of the corresponding number
+8. This functions uses all variables gathered from previous function to find the outcome of the user's input with a for loop
 
 ##### Int main () :
 
-1.
-2.
-3.
-4.
+1. This must have the while loop to loop all functions of the program with their respected variables
 
 ### Issues =
+
+1. I have having problems understanding why the program wouldn't loop properly because it would stop looping and exit the program automatically for two uses.
+   Solution: I created a loop function which uses the user's weekInput to determine if the loop should break and exit or continue
+2. I was having problem with finding a way for the program to actually find the number of days in a month in "Results" function
+   Solution: I found how to use for loops in python because during this time I just starting using python from C++. So for loops were formatted differently. However I found out from online how to set up a for loop then made a loop that adds a 1 every time the specific day comes out in the user's choice of month.
